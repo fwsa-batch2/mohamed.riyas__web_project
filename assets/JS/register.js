1ac0b5e1f5c59a4d27cf60ddf5b1110730597fd7
Mohamed-riyas2002 <rasoolmohamed669@gmail.com>
2021-12-09 15:34:21 +0530

    added JS for registration

--- assets/JS/register.js
+++ assets/JS/register.js
@@ -1,8 +1,9 @@
-let array = [];
+let user_details = [];
 function onPageLoad() {
-    const users = JSON.parse(localStorage.getItem("Riyas"));
-    if (users) {
-        array = users;
+    const getData = localStorage.getItem("Register details")
+    const users = JSON.parse(getData);
+    if (users != null) {
+        user_details = users;
     }
 }
 function register() {
@@ -11,15 +12,68 @@ function register() {
     let phone = document.getElementById("phone_no").value;
     let email = document.getElementById("email").value;
     let password = document.getElementById("password").value;
-
-    let customer_details = {
-        "Name": name,
-        "Phone": phone,
-        "Email": email,
-        "Password": password
+    let confirmPassword = document.getElementById("confirmPassword").value;
+    const phone_no = phoneChecking(phone)
+    const emailValid = emailChecking(email);
+    if (phone_no) {
+        alert("phone Number already registered");
+        return;
     }
-    array.push(customer_details);
-    let objectToArray = JSON.stringify(array);
-    localStorage.setItem("Riyas", objectToArray);
+    else if (emailValid) {
+        alert(" email already registered");
+        return;
+    }
+    else {
+        const passwordValid = password_checking(password, confirmPassword);
+        if (passwordValid) {
+            let customer_details = {
+                "Name": name,
+                "Phone": phone,
+                "Email": email,
+                "Password": password,
+                "cpassword": confirmPassword
+            }
+            user_details.push(customer_details);
+            let objectToArray = JSON.stringify(user_details);
+            localStorage.setItem("Register Details", objectToArray);
+        }
+        else {
+            alert("password mismatch")
+        }
+    }
+}
+function emailChecking(inputEmail) {
+    let email_exist = false;
+    let len = user_details.length;
+    for (i = 0; i < len; i++) {
+        let indexOf = user_details[i];
+        let gettingEmail = indexOf.Email;
+        if (inputEmail == gettingEmail) {
+            email_exist = true;
+            break;
+        }
+    }
+    return email_exist;
+}
+function password_checking(password1, password2) {
+    if (password1 != password2) {
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+function phoneChecking(inputphone) {
+    let phoneNo_exist = false;
+    let len = user_details.length;
+    for (i = 0; i < len; i++) {
+        let indexOf = user_details[i];
+        let gettingphone = indexOf.Phone;
+        if (inputphone == gettingphone) {
+            phoneNo_exist = true;
+            break;
+        }
+    }
+    return phoneNo_exist;
 }
 onPageLoad();
