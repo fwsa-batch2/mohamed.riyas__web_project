601a40a9f9e208a4704320f2822eb3836e2e77ca
Mohamed-riyas2002 <93571049+Mohamed-riyas2002@users.noreply.github.com>
2024-11-05 19:54:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WM8QNC9Z');
+
 let updateHtml = "";
 function updatePopularProducts() {
     let details= JSON.parse(localStorage.getItem("popularProducts"));
@@ -8,10 +14,3 @@ function updatePopularProducts() {
 }
 let callingPopularProducts = updatePopularProducts();
 document.getElementById("popularProducts").innerHTML = callingPopularProducts;
-
-
-(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-WM8QNC9Z');
